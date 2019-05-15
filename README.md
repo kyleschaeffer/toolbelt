3330458294f4746a072569d5331aa1ccb68644ad
kyleschaeffer <kyle@spirito.us>
2019-05-15 09:24:18 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Toolbelt: String Encoding, Decoding, and Escaping
+
+[https://tbelt.io](https://tbelt.io/)
+
+---
+
+## Development
+
+Toolbelt is a static single-page web application. Build the web application using [Parcel](https://parceljs.org/) and [Yarn](https://yarnpkg.com/):
+
+```sh
+# Build in "watch" mode, launch local dev server
+yarn run dev
+
+# Build the app in production mode
+yarn run build
+```
+
+Be sure to run in production mode before making any pull requests, as the `docs` folder is used by GitHub to host all content for [tbelt.io](https://tbelt.io/).
---
+++ src/components/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="app">
+    <main class="main" role="main">
+      <p>Hello.</p>
+    </main>
+
+    <footer class="colophon" role="contentinfo">Toolbelt by <a href="https://twitter.com/kyleschaeffer">@kyleschaeffer</a></footer>
+  </div>
+</template>
+
+<script>
+import ClipboardJS from 'clipboard'
+import queryString from 'query-string'
+
+export default {
+  components: {
+
+  },
+
+  data() {
+    return {
+
+    }
+  },
+
+  async mounted() {
+    // Copy to clipboard
+    const clipboard = new ClipboardJS('.copy')
+    clipboard.on('success', function(e) {
+      e.trigger.classList.add('copied')
+      setTimeout(() => {
+        e.trigger.classList.remove('copied')
+      }, 3000)
+      e.clearSelection()
+    })
+  },
+
+  methods: {
+    scrollTop() {
+      document.body.scrollTop = document.documentElement.scrollTop = 0
+    },
+  },
+}
+</script>
